805ed3bbf67d138b72d66f4c882570dfd24a0a21
Yashashri-Dhobe <yashashridhobe@gmail.com>
2024-07-02 19:09:56 +0530

    Multiplication.cpp
    I made  a multiplication program.

---
+++ Multiplication.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  
+  int num1 , num2 , mul;
+  cout << "enter both numer\n";
+  cin >> num1 >> num2 ;
+  mul= num1*num2 ;
+  cout << "ans is:" << " " <<mul;
+   
+    return 0;
+}
