184b75b67cbf395ef9effd3b7da6d5d376033e72
Yashashri-Dhobe <yashashridhobe@gmail.com>
2024-07-02 18:40:21 +0530

    substraction.cpp
    I made  a sunstraction program.

---
+++ substraction.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std ;
+
+int main()
+{ 
+   int  num1 , num2 , sub ;
+   cout << "enter both number\n" ;
+   cin >> num1 >> num2 ;
+   sub = num1 - num2;
+   cout << "ans is :" << " " <<sub;
+   
+
+    return 0;
+}
