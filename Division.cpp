bb6cae6f56d780e8c0f8d1e0befdc6d2384f2a46
Yashashri-Dhobe <yashashridhobe@gmail.com>
2024-07-02 19:07:45 +0530

    Division.cpp
    I made a program were i preformed division.

---
+++ Division.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  
+  int num1 , num2 , div;
+  cout << "enter both numer\n";
+  cin >> num1 >> num2 ;
+  div= num1/num2 ;
+  cout << "ans is:" << " " <<div;
+      
+    return 0;
+}
