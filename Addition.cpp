e804e43383db295f060517694a460c0fa9bfa747
Yashashri-Dhobe <yashashridhobe@gmail.com>
2024-07-02 18:17:24 +0530

    Addition.cpp
    I learned my first cpp program !

---
+++ Addition.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+
+{
+    /* here we did addition*/
+    double num1, num2 , sum , a = 0;
+    cout << "enter both numbers\n";
+    cin >> num1 >> num2;
+    sum = num1+num2 ;
+    cout <<"ans is " << sum <<" " << a;
+    
+    return 0;
+}
