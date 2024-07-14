ab8e7ed83392560e3182da7421a2b780d9205202
Yashashri-Dhobe <yashashridhobe@gmail.com>
2024-07-14 21:14:11 +0530

    Add files via upload
    Made a program that calculates the factorial of a number.

---
+++ forloop-factorial.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   
+   int num , factorial=1;
+   cout << "enter a number\n";
+   cin>> num;
+   
+   for(int i = 1; i<=num ; i++)
+   factorial*= i ;
+   cout << "factorial is " << factorial <<endl;
+   
+   return 0;
+}
